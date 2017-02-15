acdb0b5727e9df05753d39d52b821ab098810ca4
Gonza <gonzalo.medinarosa@litebyte.us>
2017-02-15 13:49:52 -0300

    Improve style

--- src/contact/contact.js
+++ src/contact/contact.js
@@ -1,4 +1,4 @@
 function adjustTextArea(textarea) {
   textarea.style.height = "1px";
-  textarea.style.height = (15 + textarea.scrollHeight) + "px";
+  textarea.style.height = (100 + textarea.scrollHeight) + "px";
 }
