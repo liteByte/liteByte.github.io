3894f54ae66ff945ef9442683574021228dc75ea
Gonza <gonzalo.medinarosa@litebyte.us>
2017-03-01 12:29:09 -0300

    Remove process line animation

--- src/process/process.js
+++ src/process/process.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 
-  var win = $(window);
   var body = $("body");
 
   body.find('.animate').each(function(i, obj) {
@@ -15,23 +14,6 @@ $(document).ready(function() {
         if (step > frames.length - 1) step = 0;
       }, 100);
     }, 750);
-  });
-
-  var process = $('#process');
-  var lines = process.find('.line');
-  win.scroll(function() {
-    var scroll = body.scrollTop() + win.height() / 3;
-    var limit = 500;
-    lines.each(function(i, obj) {
-      var el = $(obj);
-      var diff = Math.abs(el.offset().top - scroll) / 100;
-      if (diff > limit) diff = limit;
-      if (diff < 1) diff = 1;
-
-      el.css('opacity', 1 / diff);
-
-    });
-
   });
 
 });
